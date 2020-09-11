e581bad0c9270b129cfdf763723baf18542044a1
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-11 00:04:55 -0500

    async await

--- src/async/index.js
+++ src/async/index.js
@@ -1,7 +1,7 @@
 const doSomethingAsync = () => {
   return new Promise((resolve, reject) =>{
     (true)
-    ? setTimeout(()=> resolve('Do Somenthing Async'),2000)
+    ? setTimeout(()=> resolve('Do Somenthing Async'),3000)
     : reject(new Error('Test error'))
   })
 }
@@ -14,3 +14,17 @@ const doSomething = async () => {
 console.log('Before');
 doSomething();
 console.log('After');
+
+
+const anotherFuncion = async () => {
+  try {
+    const something = await doSomethingAsync()
+    console.log(something);
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log('Before 1');
+anotherFuncion();
+console.log('After 1');
